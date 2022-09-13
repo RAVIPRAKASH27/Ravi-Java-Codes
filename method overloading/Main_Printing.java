fb8b9db89606352191042d9bb1c01cf31b17e367
RAVIPRAKASH27 <112938058+RAVIPRAKASH27@users.noreply.github.com>
2022-09-13 13:03:52 +0530

    Add files via upload

---
+++ method overloading/Main_Printing.java
@@ -0,0 +1,7 @@
+class Main_Printing
+{
+	public static void main(String [] args)
+	{
+	Printing1.getPrinting(400072,"Sakinaka","Chandivali");
+	}
+}
---
+++ method overloading/Printing1.java
@@ -0,0 +1,16 @@
+class Printing1
+{
+	static void getPrinting(int n)
+	{
+	System.out.println(n);
+	}
+	static void getPrinting(int n,String m)
+	{
+	System.out.println(n+m);
+	}
+	static void getPrinting(int n,String m,String a)
+	{
+	System.out.println(n+" "+m+" "+a);
+	}
+	
+}
